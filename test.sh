e9b0bdad4a027a3c4f8df75bcb874ccaf26e2dc9
fire328 <fire328@gmail.com>
2024-06-12 18:47:10 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -1,5 +1,19 @@
-test
-test2
-the first value is 'test5454' and the second value is 'asdfinalfinal'
+#!/bin/bash
+#SBATCH -J MPNN_test
+#SBATCH -p mix_veryshort
+#SBATCH -N 1
+#SBATCH --gres=gpu:1
+#SBATCH -n 4
+#SBATCH -o %j.log
 
-test3341
+echo "running"
+export LD_LIBRARY_PATH=/appl/anaconda3/envs/SE3nv/lib/:$LD_LIBRARY_PATH
+source /appl/anaconda3/etc/profile.d/conda.sh
+conda activate /appl/anaconda3/envs/SE3nv/
+module load cuda/11.8
+#### -----Generate any Protein------####
+#/appl/RFdiffusion/scripts/run_inference.py 'contigmap.contigs=[150-150]' inference.output_prefix=test_outputs/test inference.num_designs=10
+
+
+#### -----Generate Binder ----- ####
+/appl/RFdiffusion/scripts/run_inference.py inference.input_pdb=./7ai6.pdb  'contigmap.contigs=[A2-116/0 150-200]'         'ppi.hotspot_res=[A11,A12,A13,A33,A34,A35,A36,A38,A53,A54,A56,A58,A61,A68,A70,A72,A74]'         inference.output_prefix=binder_outputs/test inference.num_designs=2
