4ed2035eefa5dbab80ebca00723abd1f9c664d75
fire328 <fire328@gmail.com>
2024-06-16 19:48:36 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -11,9 +11,10 @@ echo "running"
 export LD_LIBRARY_PATH=/appl/anaconda3/envs/diffusion/lib/:$LD_LIBRARY_PATH
 source /appl/anaconda3/etc/profile.d/conda.sh
 conda activate /appl/anaconda3/envs/diffusion/
-module load cuda/11.1
+module load cuda/11.8
 nvcc --version
 hostname -s
 nvidia-smi
 
-python -m ./rf_diffusion_all_atom/run_inference inference.deterministic=True diffuser.T=100 inference.output_prefix=output/1BC8/ inference.input_pdb=1BC8.pdb contigmap.contigs=['150-150'] inference.ligand= inference.num_designs=3 inference.design_startnum=0
+echo "python running"
+python -m /home/baelab/rfdAA/rf_diffusion_all_atom/run_inference inference.deterministic=True diffuser.T=100 inference.output_prefix=output/1BC8/ inference.input_pdb=1BC8.pdb contigmap.contigs=['150-150'] inference.ligand= inference.num_designs=3 inference.design_startnum=0
