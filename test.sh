4310cc96541d65bcc1f39665f77c115218396fd1
fire328 <fire328@gmail.com>
2024-06-16 20:46:46 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -8,15 +8,19 @@
 #SBATCH -o %j.log
 
 echo "running"
-export LD_LIBRARY_PATH=/appl/anaconda3/envs/diffusion/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/appl/anaconda3/envs/SE3nv/lib/:$LD_LIBRARY_PATH
 source /appl/anaconda3/etc/profile.d/conda.sh
-conda activate /appl/anaconda3/envs/diffusion/
+conda activate /appl/anaconda3/envs/SE3nv/
 module load cuda/11.8
 nvcc --version
 hostname -s
 nvidia-smi
 
-cd /home/baelab/rfdAA/rf_diffusion_all_atom
+cd /appl/RFdiffusion/scripts/
 echo "python running"
-python ./run_inference.py inference.deterministic=True diffuser.T=100 inference.output_prefix=/home/baelab/Heesoo/test/output/ligand_only/sample inference.input_pdb=/home/baelab/Heesoo/test/input/7v11.pdb contigmap.contigs=['150-150'] inference.ligand=OQO inference.num_designs=1 inference.design_startnum=0
+#### -----Generate any Protein------####
+python ./run_inference.py 'contigmap.contigs=[150-150]' inference.output_prefix=/home/baelab/Heesoo/test/outputs/justdiffusion/sample inference.num_designs=2
 
+
+#### -----Generate Binder ----- ####
+#/appl/RFdiffusion/scripts/run_inference.py inference.input_pdb=./7ai6.pdb  'contigmap.contigs=[A2-116/0 150-200]' #        'ppi.hotspot_res=[A11,A12,A13,A33,A34,A35,A36,A38,A53,A54,A56,A58,A61,A68,A70,A72,A74]' #        inference.output_prefix=binder_outputs/test inference.num_designs=2
