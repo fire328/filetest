098916dedb5826d87a721d83df00bc4b396d8973
fire328 <fire328@gmail.com>
2024-06-19 16:46:32 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -J RFdiffusionAATest
+#SBATCH -J MPNNtest
 #SBATCH -p mix_veryshort
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
 #SBATCH -n 4
-#SBATCH -o "RFdiffusionAATest_mix_veryshort_%j.log"
+#SBATCH -o "MPNNtest_mix_veryshort_%j.log"
 
-echo "RFdiffusionAATest mix_veryshort running"
+echo "MPNNtest mix_veryshort running"
 export LD_LIBRARY_PATH=/appl/anaconda3/envs/diffusion/lib/:$LD_LIBRARY_PATH
 source /appl/anaconda3/etc/profile.d/conda.sh
-conda activate /appl/anaconda3/envs/diffusion/
-module load cuda/11.8
+conda activate /appl/anaconda3/envs/ligandmpnn_env/
+module load cuda/12.2
 echo " "
 nvcc --version
 echo " "
@@ -18,8 +18,8 @@ hostname -s
 echo " "
 nvidia-smi
 
-cd /home/baelab/rfdAA/rf_diffusion_all_atom
+cd /appl/LigandMPNN
 echo " "
-echo "RFdiffusionAA python running"
-python ./run_inference.py inference.deterministic=True diffuser.T=25 inference.output_prefix=/home/baelab/Heesoo/test/output/result inference.input_pdb=/home/baelab/Heesoo/test/input/7v11.pdb contigmap.contigs=['150-150'] inference.ligand=OQO inference.num_designs=1 inference.design_startnum=0
+echo "LigandMPNN python running"
+python ./run.py --seed 111 --pdb_path "/home/baelab/Heesoo/test/input/test_0.pdb" --out_folder "/home/baelab/Heesoo/test/output" --chains_to_design "A"
 
