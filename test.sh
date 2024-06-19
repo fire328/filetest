e6d5f40fdfb3400ca32c8676f05ce669bc6334e6
fire328 <fire328@gmail.com>
2024-06-19 16:54:40 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 #SBATCH -n 4
 #SBATCH -o "MPNNtest_mix_veryshort_%j.log"
 
-echo "MPNNtest mix_veryshort running"
+echo "MPNNtest mix_veryshort start"
 export LD_LIBRARY_PATH=/appl/anaconda3/envs/diffusion/lib/:$LD_LIBRARY_PATH
 source /appl/anaconda3/etc/profile.d/conda.sh
 conda activate /appl/anaconda3/envs/ligandmpnn_env/
@@ -20,6 +20,8 @@ nvidia-smi
 
 cd /appl/LigandMPNN
 echo " "
-echo "LigandMPNN python running"
+echo "LigandMPNN python start"
 python ./run.py --seed 111 --pdb_path "/home/baelab/Heesoo/test/input/test_0.pdb" --out_folder "/home/baelab/Heesoo/test/output" --chains_to_design "A"
 
+echo " "
+echo "LigandMPNN python end"
