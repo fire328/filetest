a6aff1aeeb98ff625c33f5328bfa6316bfc638fd
fire328 <fire328@gmail.com>
2024-08-26 05:17:02 +0000

    commit message 01

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -J ColabDesignTest
+#SBATCH -J RFdiffusionAATest
 #SBATCH -p mix_veryshort
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
 #SBATCH -n 4
-#SBATCH -o ColabDesignTest_mix_veryshort_%j.log
+#SBATCH -o "RFdiffusionAATest_mix_veryshort_%j.log"
 
-echo "ColabDesignTest mix_veryshort start"
-export LD_LIBRARY_PATH=/appl/anaconda3/envs/SE3nv/lib/:$LD_LIBRARY_PATH
+echo "RFdiffusionAATest mix_veryshort start"
+export LD_LIBRARY_PATH=/appl/anaconda3/envs/diffusion/lib/:$LD_LIBRARY_PATH
 source /appl/anaconda3/etc/profile.d/conda.sh
-conda activate /appl/anaconda3/envs/SE3nv/
-module load cuda/12.2
+conda activate /appl/anaconda3/envs/diffusion/
+module load cuda/11.8
 echo " "
 nvcc --version
 echo " "
@@ -18,16 +18,10 @@ hostname -s
 echo " "
 nvidia-smi
 
-cd /home/baelab/Heesoo/test
+cd /home/baelab/rfdAA/rf_diffusion_all_atom
+echo " "
+echo "RFdiffusionAA python start"
+python ./run_inference.py inference.deterministic=True diffuser.T=25 inference.output_prefix=/home/baelab/Heesoo/test/output/result inference.input_pdb=/home/baelab/Heesoo/test/input/7v11.pdb contigmap.contigs=['150-150'] inference.ligand=OQO inference.num_designs=1 inference.design_startnum=0
 
 echo " "
-echo "ColabDesign python start"
-
-#### -----Generate any Protein------####
-#python /appl/RFdiffusion/scripts/run_inference.py contigmap.contigs=['150-150'] inference.output_prefix=/home/baelab/Heesoo/test/output/result inference.num_designs=1
-
-#### -----Generate Binder ----- ####
-/home/baelab/anaconda3/envs/ColabDesignHeesoo/bin/python /home/baelab/gitforder/colabdesign/rf/designability_test.py --pdb=/home/baelab/Heesoo/test/output/result_0.pdb --loc=/home/baelab/Heesoo/test/output/result --contig=A:2:-:1:1:6:/:0: :1:5:0:-:1:5:0 --copies=1 --num_seqs=1 --num_recycles=3 --rm_aa=C --mpnn_sampling_temp=0.0001 --num_designs=1 --initial_guess --use_multimer 
-
-echo " "
-echo "ColabDesign python end"
+echo "RFdiffusionAA python end"
